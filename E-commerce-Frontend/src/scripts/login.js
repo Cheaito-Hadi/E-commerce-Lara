3a08462ae95429116f0cd3800a0a799acef45c8f
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-31 00:50:22 +0300

    Restyle login and add to local storage

--- E-commerce-Frontend/src/scripts/login.js
+++ E-commerce-Frontend/src/scripts/login.js
@@ -1,7 +1,7 @@
-login = () => {
+login =  () => {
 
     const login_btn = document.getElementById("login")
-    login_btn.addEventListener('click', (e) => {
+    login_btn.addEventListener ('click', async (e) => {
         e.preventDefault()
 
         const email = document.getElementById("email_in").value;
@@ -10,16 +10,19 @@ login = () => {
         let formdata = new FormData();
         formdata.append("email", email);
         formdata.append("password", password);
-        
-        let requestOptions = {
+
+        fetch("http://127.0.0.1:8000/api/login", {
             method: 'POST',
             body: formdata,
-            redirect: 'follow'
-        };
-
-        fetch("http://127.0.0.1:8000/api/login", requestOptions)
+            })
             .then(response => response.json())
-            .then(result => console.log(result))
+            .then(data => {
+                if(data.authorization.token){
+                    localStorage.setItem('user_id', data.user.id)
+                    localStorage.setItem('usertype', data.user.role_id)
+                    window.location.href='/E-commerce-Frontend/src/pages/shop.html'
+                }
+            })
             .catch(error => console.log('error', error));
 
     })
