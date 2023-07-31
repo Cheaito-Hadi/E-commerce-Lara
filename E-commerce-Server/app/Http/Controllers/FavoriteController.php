d063bc44763dca20c8613e053f2ccad26ff50b82
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-31 19:56:52 +0300

    Show favorite php api

--- E-commerce-Server/app/Http/Controllers/FavoriteController.php
+++ E-commerce-Server/app/Http/Controllers/FavoriteController.php
@@ -10,9 +10,16 @@ use App\Models\Product;
 
 class FavoriteController extends Controller
 {
-    function getFavorites()
-    {
+    public function getFavorites(Request $request){
 
+        $favorites = Favorite::all() -> where('user_id', $request-> user_id);
+        foreach($favorites as $favorite){
+            $price = Product::find($favorite -> product_id) -> price;
+            $name = Product::find($favorite -> product_id) -> name;
+            $favorite -> name = $name;
+            $favorite -> price = $price;
+        }
+        return json_encode(["favorites" => $favorites]);
     }
 
     function addFavorites(Request $request)
--- E-commerce-Server/routes/api.php
+++ E-commerce-Server/routes/api.php
@@ -17,6 +17,6 @@ Route::post('/add_update_product/{id?}', [ProductController::class, "addOrUpdate
 Route::post('/delete_product/{id}', [ProductController::class, "deleteProduct"]);
 Route::get('/show_products', [ProductController::class, "getProducts"]);
 Route::post('/add_favorite', [FavoriteController::class, "addFavorites"]);
-Route::get('/show_favorite', [FavoriteController::class, "getFavorites"]);
+Route::post('/show_favorite', [FavoriteController::class, "getFavorites"]);
 Route::get('/show_cart', [CartController::class, "viewCart"]);
 Route::post('/add_cart', [CartController::class, "addToCart"]);
